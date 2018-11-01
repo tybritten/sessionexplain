7a40c014f8e620f423759323572ca143e1c6a073
Rowan Seymour <rowanseymour@gmail.com>
2018-10-31 21:21:31 -0500

    Use colored margin for each run

--- src/components/TimelineFrame.tsx
+++ src/components/TimelineFrame.tsx
@@ -9,8 +9,12 @@ interface Props {
 
 export class TimelineFrame extends React.Component<Props> {
 
+    private runColors: string[];
+
     constructor(props: Props) {
         super(props);
+
+        this.runColors = ['#F00', '#AA0', '#0A0', '#0AA', '#22F', '#800080', '#CD853F'];
     }
 
     public render() {
@@ -21,10 +25,11 @@ export class TimelineFrame extends React.Component<Props> {
 
         const message = `${frame.isResume ? "↩️ Resuming" : "⬇️ Starting"} run #${frame.runIndex + 1}`;
         const indent = `${frame.depth * 15}px`;
+        const color = this.runColors[frame.runIndex % this.runColors.length];
 
         return (
             <div className="Timeline-frame">
-                <div className="Frame-run-info" style={{ paddingLeft: indent }}>{message} in <i>{frame.run.flow.name}</i></div>
+                <div className="Frame-run-info" style={{ marginLeft: indent, borderLeftColor: color }}>{message} in <i>{frame.run.flow.name}</i></div>
                 <div className="Frame-steps">
                     {steps}
                 </div>
